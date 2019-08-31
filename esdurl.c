e2f9030ce8bb1b235044fdb237e9629bb450f75e
Máté Kukri <kukri.mate@gmail.com>
2019-08-31 09:59:48 +0200

    Updated esdurl

--- esdurl.c
+++ esdurl.c
@@ -149,28 +149,73 @@ struct _esd_file
 	const char *lang;
 };
 
-static void insert_before(esd_file **e, esd_file *new)
+typedef struct _esd_list esd_list;
+
+struct _esd_list
 {
-	if (!*e) { // for new list creation, not the prettiest
-		*e = new;
+	esd_file *first;
+	esd_file *last;
+};
+
+// static void insert_before(esd_list *l, esd_file *e, esd_file *new)
+// {
+// 	new->next = e;
+// 	new->prev = e->prev;
+// 	e->prev = new;
+
+// 	if (e == l->first)
+// 		l->first = new;
+// }
+
+// static void insert_after(esd_list *l, esd_file *e, esd_file *new)
+// {
+// 	new->prev = e;
+// 	new->next = e->next;
+// 	e->next = new;
+
+// 	if (e == l->last)
+// 		l->last = new;
+// }
+
+static void push_back(esd_list *l, esd_file *new)
+{
+	if (!l->last) {
+		l->first = new;
+		l->last = new;
 		return;
 	}
 
-	new->next = (*e);
-	new->prev = (*e)->prev;
-	(*e)->prev = new;
+	new->prev = l->last;
+	l->last->next = new;
+	l->last = new;
 }
 
-static void insert_after(esd_file **e, esd_file *new)
+static void delete(esd_list *l, esd_file *e)
 {
-	if (!*e) { // for new list creation, not the prettiest
-		*e = new;
-		return;
+	if (l->first == e) {
+		l->first = e->next;
+	} else {
+		e->prev->next = e->next;
 	}
 
-	new->prev = (*e);
-	new->next = (*e)->next;
-	(*e)->next = new;
+	if (l->last == e) {
+		l->last = e->prev;
+	} else {
+		e->next->prev = e->prev;
+	}
+
+	free(e);
+}
+
+static void remove_dups(esd_list *l)
+{
+	for (esd_file *i = l->first; i; i = i->next)
+		for (esd_file *j = l->first; j; j = j->next) {
+			if (i == j)
+				continue;
+			if (!strcmp(i->url, j->url))
+				delete(l, j);
+		}
 }
 
 /* simple doubly-linked list implementation END */
@@ -207,11 +252,13 @@ int main()
 		goto err2;
 	}
 
-	esd_file *first = NULL;
+	esd_list list;
+	memset(&list, 0, sizeof(esd_list));
 
 	for (xmlNode *file = files->children; file; file = file->next) {
 		if (!strcmp(file->name, "File")) {
-			esd_file *current = malloc(sizeof(esd_file));
+			// warning this has to be calloc, otherwise it will cause nightmares
+			esd_file *current = calloc(sizeof(esd_file), 1);
 
 			for (xmlNode *node = file->children; node; node = node->next) {
 				if (!strcmp(node->name, "FilePath"))
@@ -222,17 +269,24 @@ int main()
 					current->arch = xmlNodeGetContent(node);
 			}
 
-			insert_after(&first, current);
+			push_back(&list, current);
 		}
 	}
 
-	if (!first) {
+	if (!list.first) {
 		fprintf(stderr, "No ESD files were found\n");
 		goto err2;
 	}
 
-	for (esd_file *cur = first; cur; cur = cur->next)
-		printf("%s\n", cur->url);
+	// remove duplicates from the list
+	remove_dups(&list);
+
+	// print the list
+	for (esd_file *cur = list.first; cur; cur = cur->next) {
+		printf("Language: %s\nArchitecture: %s\nURL: %s\n\n",
+			cur->lang, cur->arch, cur->url);
+		free(cur);
+	}
 
 	xmlFreeDoc(doc);
 	xmlCleanupParser();
